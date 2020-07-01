44e606e78a10b4c65163c8805586fd4940ef28b0
Marina Pavlova <marina.torgasheva@gmail.com>
2020-07-01 11:14:15 +0500

    Create 'auction' app

---
+++ auction/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Items
+class Item(models.Model):
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+         return self.title
+
+    title = models.CharField(max_length=255)
+    create_dt = models.DateTimeField(null=False, verbose_name='Created at')
+    close_dt = models.DateTimeField(null=False, verbose_name='Closing at')
+    start_bid = models.DecimalField(default=0.00, decimal_places=2, max_digits=20, verbose_name='Start bid, $')
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=20, verbose_name='Price, $')
+    description = models.CharField(max_length=255, null=True)
