dd9e9d1b852b6b1adea1772fb0be9781fba5582d
Marina Pavlova <marina.torgasheva@gmail.com>
2020-07-01 16:58:34 +0500

    Add item update

--- auction/urls.py
+++ auction/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.show_items, name='show_items'),
     path('items', views.items, name='items'),
+    path('items/<int:pk>', views.item_update, name='item_update'),
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.serializers.json import DjangoJSONEncoder
 import json
@@ -11,6 +11,8 @@ def index(request):
 def show_items(request):
     return render(request, 'items.html')
 
+
+# Create an item or get a list of items
 def items(request):
     if request.method == 'POST':
         add_item(request)
@@ -33,6 +35,7 @@ def items(request):
 
         return HttpResponse(items_json, content_type="text/json")
 
+# Create an item
 def add_item(request):
     data = json.loads(request.body.decode('utf-8'))
     data['close_dt'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
@@ -40,3 +43,22 @@ def add_item(request):
 
     context = {"newID": new_item.id}
     return HttpResponse(json.dumps(context), content_type="text/json")
+
+# Updating an item
+def item_update(request, pk):
+    data = json.loads(request.body.decode('utf-8'))
+
+    if pk and pk != 'null':
+        item = get_object_or_404(Item, pk=pk)
+    else:
+        return HttpResponse("Item is undefined")
+
+    item.title = data.get('title') or item.title
+    item.description = data.get('description') or item.description
+    item.price = data.get('price') or item.price
+    item.close_dt = data.get('close_dt') or item.close_dt
+    item.save()
+
+    result = {"result": True}
+
+    return HttpResponse(result, content_type="text/json")
