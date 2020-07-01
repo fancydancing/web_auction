b76d11952b8a1a5a59be14e5e0ff60cdb2085888
Marina Pavlova <marina.torgasheva@gmail.com>
2020-07-01 16:33:57 +0500

    Show items list at root

--- auction/urls.py
+++ auction/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.show_items, name='show_items'),
     path('items', views.items, name='items'),
-    path('base/', views.show_items, name='show_items'),
 ]
