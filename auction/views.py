de53f7a026f47268d1e99db706aa285522c69db1
Marina Pavlova <marina.torgasheva@gmail.com>
2020-07-07 14:56:49 +0500

    Backend: - Add django forms for validation (add item, get item list) - Add timezone when converting close_dt from timestamp
    Frontend:
    - Title changed from 'input' to 'textarea'

--- auction/views.py
+++ auction/views.py
@@ -6,32 +6,54 @@ from django.core.serializers.json import DjangoJSONEncoder
 
 from .auction import AuctionItem, Authorization, AuctionList
 from .models import Item
+from .forms import ItemListForm, ItemForm
 from .deploy_db import deploy_data
 
 
 def add_item(request) -> HttpResponse:
+    """
+    Add new item.
+
+    Parameters in request:
+        title: str - item title
+        description: str - item description
+        close_dt: int - closing time for item
+        price: int - item price
+    """
     data = json.loads(request.body.decode('utf-8'))
-    params = ['title', 'description', 'close_dt', 'price']
-    missing_params = [p for p in params if p not in data]
-
-    if len(missing_params) > 0:
-        result = {'result': False, 'msg': 'Missing parameters: ' + ', '.join(missing_params)}
-    elif data['price'] <= 0:
-        result = {'result': False, 'msg': 'Price must be greater than 0.'}
-    else:
-        new_id = AuctionItem().add(data)
+    item_form = ItemForm(data)
+    if item_form.is_valid():
+        new_id = AuctionItem().add(item_form.cleaned_data)
         result = {'result': True, 'id': new_id}
+    else:
+        print(item_form.errors.as_data())
+        result = {'result': False, 'msg': item_form.errors.as_data()}
 
-    return HttpResponse(json.dumps(result), content_type="text/json")
+    return HttpResponse(json.dumps(result), content_type='text/json')
 
 
 def get_items_list(request) -> HttpResponse:
-    items_list = AuctionList(request.GET).get_list()
-    items_json = json.dumps(items_list)
-    return HttpResponse(items_json, content_type="text/json")
+    """
+    Get a list of items.
+
+    Parameters in request:
+        page: int - number of page
+        page_size: int - size of page
+        sort: str - 'asc' or 'desc'
+        order: str - field name to sort on
+        search_string: str - string to find in title or description
+        show_closed: bool - show closed items or not
+    """
+    items_form = ItemListForm(request.GET)
+    if items_form.is_valid():
+        items_list = AuctionList(items_form.cleaned_data).get_list()
+        result = json.dumps(items_list)
+    else:
+        result = {'items': []}
+    return HttpResponse(result, content_type='text/json')
 
 
-def items_view(request):
+def items_view(request) -> HttpResponse:
     """
     Operations with items depending on HTTP method.
 
@@ -43,8 +65,47 @@ def items_view(request):
     else:
         return get_items_list(request)
 
+def update_item(request, pk: int) -> HttpResponse:  
+    """
+    Update an item.
 
-def item_info_view(request, pk):
+    Parameters in request (all optional):
+        title: str - new item title
+        description: str - new item description
+        price: int - new item price
+        close_dt: int - new closing time
+    """
+    data = json.loads(request.body.decode('utf-8'))
+    # res = {}
+    if 'price' in data and data['price'] <= 0:
+        res = {'result': False, 'msg': 'Price must be greater than 0.'}
+    else:
+        result = AuctionItem(pk).edit(data)
+        res = {'result': result}
+    return HttpResponse(json.dumps(res), content_type='text/json')
+
+
+def delete_item(pk: int) -> HttpResponse:
+    """
+    Delete an item.
+
+    pk: int - item ID
+    """
+    result = AuctionItem(pk).delete()
+    return HttpResponse(json.dumps({'result': result}), content_type='text/json')
+
+
+def read_item(pk: int) -> HttpResponse:
+    """
+    Read an item.
+
+    pk: int - item ID
+    """
+    result = AuctionItem(pk).read()
+    return HttpResponse(json.dumps(result),content_type='text/json')
+
+
+def item_info_view(request, pk) -> HttpResponse:
     """
     Operations with an existing item depending on HTTP method.
 
@@ -54,63 +115,69 @@ def item_info_view(request, pk):
     """
     # Edit item
     if request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        if 'price' in data and data['price'] <= 0:
-            result = {'result': False, 'msg': 'Price must be greater than 0.'}
-            return HttpResponse(json.dumps(result), content_type="text/json")
-        result = AuctionItem(pk).edit(data)
-        return HttpResponse(json.dumps({"result": result}), content_type="text/json")
+        return update_item(request, pk)
     # Delete item
     elif request.method == 'DELETE':
-        result = AuctionItem(pk).delete()
-        return HttpResponse(json.dumps({"result": result}), content_type="text/json")
+        return delete_item(pk)
     # Read item
     elif request.method == 'GET':
-        result = AuctionItem(pk).read()
-        return HttpResponse(
-            json.dumps(result, cls=DjangoJSONEncoder),
-            content_type="text/json"
-        )
+        return read_item(pk)
 
 
-def sign_in_view(request):
-    """Authorize user and return role and username.
+def sign_in_view(request) -> HttpResponse:
+    """
+    Authorize user and return role and username.
 
-    request parameters:
-    [login]
-    [password]
+    Parameters in request:
+        login: str - user login
+        password: str - user password
     """
     data = json.loads(request.body.decode('utf-8'))
     result = Authorization().login(data)
-    return HttpResponse(json.dumps(result), content_type="text/json")
+    return HttpResponse(json.dumps(result), content_type='text/json')
 
 
-def item_bids_view(request, pk: int):
-    """Read bids/set new bid for an item"""
+def set_bid(request, pk: int) -> HttpResponse:
+    """
+    Set new bid.
+
+    Parameters in request:
+        price: int - bid sum
+        user_name: str - user login
+    """
+    data = json.loads(request.body.decode('utf-8'))
+    # Validate parameters
+    if 'price' not in data:
+        result = {'result': False, 'msg': 'Price is required.'}
+    elif 'user_name' not in data:
+        result = {'result': False, 'msg': 'Username is required.'}
+    else:
+        result = AuctionItem(pk).set_bid(data)
+    return HttpResponse(json.dumps(result), content_type='text/json')
+
+
+def bids_list(pk: int) -> HttpResponse:
+    """
+    Get a list of all bids for an item.
+
+    pk: int - item ID
+    """
+    bids_list = AuctionItem(pk).get_bids()
+    return HttpResponse(json.dumps(bids_list), content_type='text/json')
+
+
+def item_bids_view(request, pk: int) -> HttpResponse:
+    """Read bids/set new bid for an item."""
     # Set bid
     if request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        # Validate parameters
-        if 'price' not in data:
-            result = {'result': False, 'msg': 'Price is required.'}
-            return HttpResponse(json.dumps(result), content_type="text/json")
-
-        if 'user_name' not in data:
-            result = {'result': False, 'msg': 'Username is required.'}
-            return HttpResponse(json.dumps(result), content_type="text/json")
-
-        result = AuctionItem(pk).set_bid(data)
-        return HttpResponse(json.dumps(result), content_type="text/json")
+        return set_bid(request, pk)
     # Get bids list
     elif request.method == 'GET':
-        bids_list = AuctionItem(pk).get_bids()
-        bids_json = json.dumps(bids_list, cls=DjangoJSONEncoder)
-        return HttpResponse(bids_json, content_type="text/json")
+        return bids_list(pk)
 
 
-
-def index_view(request):
-    """Show start page with items list"""
+def index_view(request) -> HttpResponse:
+    """Show start page with items list."""
     deploy_data()
     return render(request, 'items.html')
 
