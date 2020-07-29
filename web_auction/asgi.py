1fe290d7b3a49f6979af569350f71108f53bbec5
Marina Pavlova <marina.torgasheva@gmail.com>
2020-07-29 17:01:58 +0500

    Changes in asgi file

--- web_auction/asgi.py
+++ web_auction/asgi.py
@@ -1,3 +1,12 @@
-from channels.layers import get_channel_layer
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
 
-channel_layer = get_channel_layer()
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+application = get_default_application()
